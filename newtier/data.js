77dccf7b37ea7e83696760c79b304e0950d822aa
quetzle <buizbuizbuizbuiz@gmail.com>
2017-07-01 01:20:46 -0400

    test page for 3.0

---
+++ newtier/data.js
@@ -0,0 +1,106 @@
+let games =
+[
+	{
+		"custom": false,
+		"name": "Smash 4",
+		"imageSets": [
+			{"folder": "ssb4", "name": "Screenshots"},
+			{"folder": "ssb4r", "name": "Renders"}
+		],
+		"characters": [
+			{"name": "mario", "main": true, "imageSets": [true,true]},
+			{"name": "luigi", "main": true, "imageSets": [true,true]},
+			{"name": "peach", "main": true, "imageSets": [true,true]},
+			{"name": "bowser", "main": true, "imageSets": [true,true]},
+			{"name": "yoshi", "main": true, "imageSets": [true,true]},
+			{"name": "rosalinaluma", "main": true, "imageSets": [true,true]},
+			{"name": "bowserjr", "main": true, "imageSets": [true,true]},
+			{"name": "wario", "main": true, "imageSets": [true,true]},
+			{"name": "donkeykong", "main": true, "imageSets": [true,true]},
+			{"name": "diddykong", "main": true, "imageSets": [true,true]},
+			{"name": "mrgamewatch", "main": true, "imageSets": [true,true]},
+			{"name": "littlemac", "main": true, "imageSets": [true,true]},
+			{"name": "link", "main": true, "imageSets": [true,true]},
+			{"name": "zelda", "main": true, "imageSets": [true,true]},
+			{"name": "sheik", "main": true, "imageSets": [true,true]},
+			{"name": "ganondorf", "main": true, "imageSets": [true,true]},
+			{"name": "toonlink", "main": true, "imageSets": [true,true]},
+			{"name": "samus", "main": true, "imageSets": [true,true]},
+			{"name": "zerosuitsamus", "main": true, "imageSets": [true,true]},
+			{"name": "pit", "main": true, "imageSets": [true,true]},
+			{"name": "palutena", "main": true, "imageSets": [true,true]},
+			{"name": "marth", "main": true, "imageSets": [true,true]},
+			{"name": "ike", "main": true, "imageSets": [true,true]},
+			{"name": "robin", "main": true, "imageSets": [true,true]},
+			{"name": "duckhunt", "main": true, "imageSets": [true,true]},
+			{"name": "kirby", "main": true, "imageSets": [true,true]},
+			{"name": "kingdedede", "main": true, "imageSets": [true,true]},
+			{"name": "metaknight", "main": true, "imageSets": [true,true]},
+			{"name": "fox", "main": true, "imageSets": [true,true]},
+			{"name": "falco", "main": true, "imageSets": [true,true]},
+			{"name": "pikachu", "main": true, "imageSets": [true,true]},
+			{"name": "charizard", "main": true, "imageSets": [true,true]},
+			{"name": "lucario", "main": true, "imageSets": [true,true]},
+			{"name": "jigglypuff", "main": true, "imageSets": [true,true]},
+			{"name": "greninja", "main": true, "imageSets": [true,true]},
+			{"name": "rob", "main": true, "imageSets": [true,true]},
+			{"name": "ness", "main": true, "imageSets": [true,true]},
+			{"name": "captainfalcon", "main": true, "imageSets": [true,true]},
+			{"name": "villager", "main": true, "imageSets": [true,true]},
+			{"name": "olimar", "main": true, "imageSets": [true,true]},
+			{"name": "wiifittrainer", "main": true, "imageSets": [true,true]},
+			{"name": "shulk", "main": true, "imageSets": [true,true]},
+			{"name": "drmario", "main": true, "imageSets": [true,true]},
+			{"name": "darkpit", "main": true, "imageSets": [true,true]},
+			{"name": "lucina", "main": true, "imageSets": [true,true]},
+			{"name": "pacman", "main": true, "imageSets": [true,true]},
+			{"name": "megaman", "main": true, "imageSets": [true,true]},
+			{"name": "sonic", "main": true, "imageSets": [true,true]},
+			{"name": "mewtwo", "main": true, "imageSets": [true,true]},
+			{"name": "lucas", "main": true, "imageSets": [true,true]},
+			{"name": "roy", "main": true, "imageSets": [true,true]},
+			{"name": "ryu", "main": true, "imageSets": [true,true]},
+			{"name": "cloud", "main": true, "imageSets": [true,true]},
+			{"name": "corrin", "main": true, "imageSets": [true,true]},
+			{"name": "bayonetta", "main": true, "imageSets": [true,true]},
+			{"name": "miibrawler1", "main": false, "imageSets": [true,true]},
+			{"name": "miigunner1", "main": false, "imageSets": [true,true]},
+			{"name": "miisword1", "main": false, "imageSets": [true,true]},
+			{"name": "miibrawler", "main": false, "imageSets": [true,true]},
+			{"name": "miigunner", "main": false, "imageSets": [true,true]},
+			{"name": "miisword", "main": false, "imageSets": [true,true]}
+		]
+	},
+	{
+		"custom": false,
+		"name": "Melee",
+		"imageSets": [
+			{"folder": "melee", "name": "Screenshots"}
+		],
+		"characters": [
+		]
+	}
+];
+
+let Game = `
+{
+	"custom": = false,
+	"name": "",
+	"imageSets": [],
+	"defaultChars": [],
+	"extraChars": []
+}
+`;
+
+let Save = `
+{
+	"name": "",
+	"custom": = false,
+	"game": = 0,
+	"tiers": [
+		{"label": "", "color": "", "characters": [
+			{"folder": "", "name": "", "label": "", "url": ""}
+		]}
+	]
+}
+`;
---
+++ newtier/tier.js
@@ -0,0 +1,80 @@
+/***
+
+	Tier List Maker v3.0
+		by radbot
+	
+*/
+
+var currentGameId;			// id of game the user is making a tier list for
+var currentImageSetId;		// id of image set the user is using
+var gameList = [];			// list of game options, used for filling dropdowns
+var imageSetList = [];		// list of image set options for current game, used for filling dropdowns
+var characterList = [];		// list of character images, used for filling tier lists or during setup
+
+var charHTML = "<div class=\"character\"><div class=\"char-controls\"><span class=\"char-settings\">+</span><span class=\"left\">&#9668;</span><span class=\"up\">&#9650;</span><span class=\"down\">&#9660;</span><span class=\"right\">&#9658;</span></div></div>";		// html for a character icon
+var colors = ["#FF7F7F","#FFBF7F","#FFDF7F","#FFFF7F","#BFFF7F","#7FFF7F","#7FFFFF","#7FBFFF","#7F7FFF","#FF7FFF","#BF7FBF","#3B3B3A","#858585","#CFCFCF","#F7F7F7"];		// default tier label colors
+
+function addCharacter(id, row, name) {
+	console.log(id + " " + name);
+	var currentRow = (row > -1 ) ? $(".tier:eq(" + row + ")") : $(".bin");	// if `row` is -1, add images to default bin at the bottom
+	var imageSet = games[currentGameId].imageSets[currentImageSetId].folder;
+	$(charHTML).css("background-image","url('img/" + imageSet + "/" + name + ".png')")
+			   .attr("id",id)
+			   .appendTo(currentRow);
+}
+
+$(document).ready(function() {
+	console.log("Page loaded.");
+	console.log(games);
+	
+	// load games into dropdown menu
+	for (var i in games)
+		gameList.push("<option value=\"" + i + "\">" + games[i].name + "</option>");
+	$("#game-select").append(gameList.join(""));
+	
+	$(document).on("click", "#go", function() {
+		characterList = [];
+		currentImageSetId = Number($("#imageset-select option:selected").val());
+		for (var i in games[currentGameId].characters) {
+			addCharacter(i, -1, games[currentGameId].characters[i].name);
+		}
+	});
+	
+	$(document).on("change", "#game-select", function() {
+		currentGameId = Number($("#game-select option:selected").val());
+		console.log(currentGameId);
+		imageSetList = [];
+		for (var i in games[currentGameId].imageSets)
+			imageSetList.push("<option value=\"" + i + "\">" + games[currentGameId].imageSets[i].name + "</option>");
+		$("#imageset-select").append(imageSetList.join(""));
+	});
+	$(document).on("change", "#imageset-select", function() {
+		currentImageSetId = Number($("#imageset-select option:selected").val());
+	});
+	
+	// move character icon with buttons instead of drag-and-drop
+	$(document).on("click", ".left", function() {
+		console.log("left");
+		$(this).closest(".character").insertBefore($(this).closest(".character").prev());
+	});
+	$(document).on("click", ".right", function() {
+		console.log("right");
+		$(this).closest(".character").insertAfter($(this).closest(".character").next());
+	});
+	$(document).on("click", ".up", function() {
+		console.log("up");
+		$(this).closest(".character").appendTo($(this).closest(".row").prevAll(".row:first").children(".tier"));
+	});
+	$(document).on("click", ".down", function() {
+		console.log("down");
+		$(this).closest(".character").prependTo($(this).closest(".row").nextAll(".row:first").children(".tier"));
+	});
+	
+	// drag and drop
+	dragula({
+		isContainer: function (el) {
+			return el.classList.contains('tier');
+		},
+		direction: 'horizontal'
+	});
+});
